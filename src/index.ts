3eceee70e25661df200b773c08dca3117061201d
Mark Dietzer <git@doridian.net>
2020-11-20 14:56:29 -0800

    fix

--- src/index.ts
+++ src/index.ts
@@ -136,7 +136,6 @@ const srv = createServer((_req, res) => {
         const v = LAST_STATUS_MAP[k];
         const i = ITEMS_MAP[k];
         if (!i) {
-            delete LAST_STATUS_MAP[k];
             continue;
         }
         htmlArray.push(`<tr>
