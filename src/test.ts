d9c45156216c1b6c158e54a9b6653f09866bf8f8
Mark Dietzer <git@doridian.net>
2020-11-28 22:34:16 -0800

    rm chat

--- src/test.ts
+++ src/test.ts
@@ -1,14 +1,1 @@
 require('dotenv').config();
-
-import { YouTubeChat } from "./livechat/youtube";
-
-async function main() {
-    const yt = new YouTubeChat({
-        channelId: 'UCs9EGYVhFw--tRN0VESceMA',
-        searchString: '5950X',
-        name: 'YouTube Falcodrin',
-    });
-    await yt.findChat();
-}
-
-main().catch((e) => console.error(e));
