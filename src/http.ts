392b960a9a0e5635534691073beec92f9589d8ed
Mark Dietzer <git@doridian.net>
2021-01-08 12:24:52 -0800

    rm some headers

--- src/http.ts
+++ src/http.ts
@@ -34,14 +34,8 @@ export async function fetchCustom(item: Item, itemStatus: Status) {
     curl.setOpt('HTTPHEADER', [
         'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
         'accept-language: en-US,en;q=0.9',
-        'sec-fetch-dest: document',
-        'sec-fetch-mode: navigate',
-        'sec-fetch-site: none',
-        'sec-fetch-user: ?1',
         'upgrade-insecure-requests: 1',
-        `user-agent: ${getUserAgent()}`,
-        'cache-control: no-cache',
-        'pragma: no-cache',
+        `user-agent: ${getUserAgent()}`
     ]);
     curl.setOpt('TIMEOUT_MS', softTimeout);
     if (item.needProxy) {
