130abd6ab650da2abcbe331a99d87a47f41444f3
Doridian <git@doridian.net>
2022-10-14 10:42:01 -0700

    add 4090 and bandh, but cloudflare works too good

--- src/http.ts
+++ src/http.ts
@@ -20,7 +20,7 @@ export class HttpError extends Error {
 }
 
 function getUserAgent() {
-    return 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.67 Safari/537.36 Edg/87.0.664.47';
+    return 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36';
 }
 
 export async function fetchCustom(item: Item, itemStatus: Status) {
---
+++ src/stores/impl/bandh.ts
@@ -0,0 +1,18 @@
+import { StoreItemConfig, StoreItemDescConfig } from '../index.js';
+
+export function factory(cfg: StoreItemConfig) {
+    return {
+        url: `https://www.bhphotovideo.com/c/product/${cfg.sku}/${(cfg as StoreItemDescConfig).desc}.html`,
+        dataType: 'text',
+        matcher: 'text_contains',
+        path: '',
+        value: 'data-selenium="stockStatus">In Stock</span>',
+        notifyOnResult: true,
+        needProxy: true
+    };
+}
+
+export const test = {
+    sku: '751035-REG',
+    desc: 'Pearstone_hda_106_Standard_Series_HDMI_to',
+};
